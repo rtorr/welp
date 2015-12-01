a3e8809ae8ed6190130d25e3005cf22e95c46570
Richard Torruellas <rtorruellas@gmail.com>
2015-12-01 09:34:19 -0700

    adding coverage

--- karma.conf.js
+++ karma.conf.js
@@ -11,13 +11,18 @@ module.exports = function(config) {
 
     browsers: [ 'PhantomJS' ],
     frameworks: [ 'mocha' ],
-    reporters: [ 'mocha' ],
+    reporters: [ 'mocha', 'coverage', 'coveralls' ],
 
     files: [
       'node_modules/es5-shim/es5-shim.js',
       'node_modules/es5-shim/es5-sham.js',
       'tests.webpack.js'
     ],
+
+    coverageReporter: {
+      type: 'lcov', // lcov or lcovonly are required for generating lcov.info files
+      dir: 'coverage/'
+    },
 
     preprocessors: {
       'tests.webpack.js': [ 'webpack', 'sourcemap' ]
