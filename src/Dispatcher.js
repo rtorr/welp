c1e1e72f2f4a76317f94ae637b278c8cf277baa5
Richard Torruellas <rtorruellas@gmail.com>
2016-01-28 16:31:18 -0700

    updating dispatcher

--- src/Dispatcher.js
+++ src/Dispatcher.js
@@ -1,5 +1,29 @@
 import { Dispatcher } from 'flux';
+import objectAssign from 'object-assign';
+import queue from 'queue';
 
-const WelpDispatcher = new Dispatcher();
+var _action_queue = queue({
+  concurrency: 1
+});
+
+var WelpDispatcher = new Dispatcher();
+
+objectAssign(WelpDispatcher, {
+  _dispatch: WelpDispatcher.dispatch,
+
+  /**
+   * @param {object} action
+   */
+  dispatch: function(action) {
+
+    _action_queue.push(function(cb) {
+      this._dispatch(action);
+      cb();
+    }.bind(this));
+
+    _action_queue.start();
+
+  }
+});
 
 export default WelpDispatcher;
